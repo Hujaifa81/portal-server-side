511399fde915d749ac1e0f8e8659dbb0481959a5
Hujaifa81 <abuhojaifa123@gmail.com>
2025-03-27 00:24:41 +0600

    all post endpoint completed

--- index.js
+++ index.js
@@ -51,36 +51,6 @@ async function run() {
       const result = await movieCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     })
-    app.post('/add', async (req, res) => {
-      const data = req.body;
-      const result = await movieCollection.insertOne(data);
-      res.send(result)
-    })
-    app.put('/update/:id', async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updatedMovie = {
-        $set: {
-          title: data.title,
-          genres: data.genres,
-          year: data.year,
-          rating: data.rating,
-          duration: parseFloat(data.duration),
-          summary: data.summary,
-          poster: data.poster,
-          email: data.email
-        }
-      }
-      const result = await movieCollection.updateOne(filter, updatedMovie, options);
-      res.send(result)
-    })
-    app.delete('/delete/:id', async (req, res) => {
-      const id = req.params.id;
-      const result = await movieCollection.deleteOne({ _id: new ObjectId(id) });
-      res.send(result)
-    })
     app.get('/favorite-movies/:email', async (req, res) => {
       const email = req.params.email;
       const cursor = favoriteMovieCollection.find({ email });
@@ -92,26 +62,19 @@ async function run() {
       const result = await favoriteMovieCollection.findOne({ email, movieId });
       res.send(result);
     })
+    app.post('/add', async (req, res) => {
+      const data = req.body;
+      const result = await movieCollection.insertOne(data);
+      res.send(result)
+    })
     app.post('/add-favorite', async (req, res) => {
       const data = req.body;
       const result = await favoriteMovieCollection.insertOne(data);
       res.send(result)
     })
-    app.delete('/delete-favoriteMovie/:id', async (req, res) => {
 
-        
-      const id = req.params.id;
-      
-      const result = await favoriteMovieCollection.deleteOne({ _id: new ObjectId(id) });
-      res.send(result)
-    })
-    app.delete('/delete-favoriteMovie-with-email-movieId', async (req, res) => {
-
-      const { email, movieId } = req.query;
-     
-      const result = await favoriteMovieCollection.deleteOne({ email, movieId });
-      res.send(result)
-    })
+    
+   
 
   } finally {
     // Ensures that the client will close when you finish/error
